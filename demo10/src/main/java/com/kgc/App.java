4e87cccf6787e1d0745288fef49c49ea0db3683b
wangxinyu1008 <2401816735@qq.com>
2020-09-15 22:09:58 +0800

    测试10

---
+++ demo10/src/main/java/com/kgc/App.java
@@ -0,0 +1,14 @@
+package com.kgc;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println("测试10");
+    }
+}
